31c3bf03e35b9c1b13b8fb1f3d180bf7917e23a1
matin907734-ctrl <Matin907734@gmail.com>
2026-07-15 22:58:23 +0330

    Session 2: Jinja2 templates, static files, base layout

--- shop-project/app.py
+++ shop-project/app.py
@@ -1,20 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Hello<h1/>' \
-    '<h2>wellcome to my first flask website</h2>'
+    return render_template('index.html', name='matin', age='15')
 
 @app.route('/about')
 def about():
-    return '.این صفحه ی دربارهی ماست' 
+    return render_template('about.html')
 
 @app.route('/contact')
 def contact():
     return '.برای تماس با ما به این آدرس ایمیل بزنید' 
 
+@app.route("/me")
+def me():
+    return "i'm Matin"
+
+@app.route('/subjects')
+def subjects():
+    topics = ['Html', 'css', 'javascript', 'python']
+    return render_template('subjects.html', topics=topics)
 
 if __name__ == '__main__':
     app.run(debug=True)
