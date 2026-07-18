6b9020ed1f4a405163ccd913a94948d7b6d01584
matin907734-ctrl <Matin907734@gmail.com>
2026-07-18 15:43:46 +0330

    Session 3: forms, GET/POST, validation, flash messages

--- shop-project/app.py
+++ shop-project/app.py
@@ -1,6 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash, redirect, url_for
 
 app = Flask(__name__)
+app.secret_key = 'یک-رشته-تصادفی-و-مخفی'
 
 @app.route('/')
 def home():
@@ -14,9 +15,30 @@ def about():
 def contact():
     return '.برای تماس با ما به این آدرس ایمیل بزنید' 
 
-@app.route("/me")
-def me():
-    return "i'm Matin"
+@app.route('/register', methods=['GET','POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        age = request.form.get('age')
+        if not username:
+            flash('you forgot the name')
+            return redirect(url_for('register'))
+        if not age:
+            flash('you forgot the age')
+            return redirect(url_for('register'))
+        flash(f'welcome {username}!you succsed')
+        return redirect(url_for('register'))
+    return render_template('register.html')
+
+@app.route('/search', methods=['GET','POST'])
+def search():
+    if request.method == 'POST':
+        query = request.args.get('q', '')
+        search = request.form.get('Search')
+        if query == search:
+            flash(f'the results for search {search}')
+            return redirect(url_for('search'))
+    return render_template('search.html')
 
 @app.route('/subjects')
 def subjects():
